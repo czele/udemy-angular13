e31cbee02c782f8822430bad98fe703e3c25cc88
Camila Zelenovsky <camila.zelenovsky@cat.com>
2022-10-13 13:40:04 -0300

    CRUD tarefa-service finalizado

--- gerenciador-de-tarefas/src/app/tarefas/shared/tarefa.service.ts
+++ gerenciador-de-tarefas/src/app/tarefas/shared/tarefa.service.ts
@@ -40,4 +40,12 @@ export class TarefaService {
     localStorage['tarefas'] = JSON.stringify(tarefas);
   }
 
+    alterarStatus(id: number): void {
+      const tarefas: Tarefa[] = this.listarTodos();
+      tarefas.forEach((obj, index, objs) => {
+        if (id === obj.id) {
+          objs[index].concluida = !obj.concluida;
+        }});
+      localStorage['tarefas'] = JSON.stringify(tarefas);
+    }
 }
