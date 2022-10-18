57d2db608ad2559fd54b0d0b143920cd331afdc5
Camila Zelenovsky <camila.zelenovsky@cat.com>
2022-10-18 13:26:27 -0300

    remover concluido

--- gerenciador-de-tarefas/src/app/tarefas/listar/listar-tarefa.component.ts
+++ gerenciador-de-tarefas/src/app/tarefas/listar/listar-tarefa.component.ts
@@ -21,4 +21,12 @@ export class ListarTarefaComponent implements OnInit {
     return this.tarefaService.listarTodos();
   }
 
+  remover($event: any, tarefa: Tarefa): void {
+    //Quando clica o event.preventDefault impede que o localhost se atualize para não quebrar a remoção corretamente
+    $event.preventDefault();
+    if (confirm('Deseja remover a tarefa "' + tarefa.nome + '"?'))
+      this.tarefaService.remover(tarefa.id);
+      this.tarefas = this.listarTodos();
+  }
+
 }
