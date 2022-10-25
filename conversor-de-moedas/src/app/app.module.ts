de932d610e95b720bf721f0bca841fdc56b82a3c
Camila Zelenovsky <camila.zelenovsky@cat.com>
2022-10-25 15:54:37 -0300

    criação dos models

--- conversor-de-moedas/src/app/app.module.ts
+++ conversor-de-moedas/src/app/app.module.ts
@@ -1,8 +1,8 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ConversorModule } from './conversor/conversor.module';
 
 @NgModule({
   declarations: [
@@ -10,7 +10,8 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ConversorModule
   ],
   providers: [],
   bootstrap: [AppComponent]
