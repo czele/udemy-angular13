e39c496fc4cdfe6e0eabd66b659ca23dda84979a
Camila Zelenovsky <camila.zelenovsky@cat.com>
2022-11-01 11:13:52 -0300

    Finalizado

--- jogo-da-velha/src/app/app.module.ts
+++ jogo-da-velha/src/app/app.module.ts
@@ -1,9 +1,9 @@
-import { JogoDaVelhaModule } from './jogo-da-velha/jogo-da-velha.module';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
+
 import { AppComponent } from './app.component';
+import { JogoDaVelhaModule } from './jogo-da-velha/jogo-da-velha.module';
 
 @NgModule({
   declarations: [
