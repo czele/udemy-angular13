706a358e93b2df8f5867621d6fb7458dc0ebbbb9
Camila Zelenovsky <camila.zelenovsky@cat.com>
2022-11-01 13:05:17 -0300

    Criação do modulo, component e service

--- dashboard/src/app/app.component.spec.ts
+++ dashboard/src/app/app.component.spec.ts
@@ -1,3 +1,5 @@
+import { DadosService } from './dashboard/dados.service';
+import { DashboardModule } from './dashboard/dashboard.module';
 import { TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
@@ -6,11 +8,15 @@ describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [
-        RouterTestingModule
+        RouterTestingModule,
+        DashboardModule
       ],
       declarations: [
         AppComponent
       ],
+      providers: [
+        DadosService,
+      ]
     }).compileComponents();
   });
 
@@ -20,10 +26,10 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'dashboard'`, () => {
+  it(`should have as title 'Dashboard'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('dashboard');
+    expect(app.title).toEqual('Dashboard');
   });
 
   it('should render title', () => {
--- dashboard/src/app/app.module.ts
+++ dashboard/src/app/app.module.ts
@@ -3,6 +3,8 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DashboardModule } from './dashboard/dashboard.module';
+import { DadosService } from './dashboard/dados.service';
 
 @NgModule({
   declarations: [
@@ -10,9 +12,12 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    DashboardModule
   ],
-  providers: [],
+  providers: [
+    DadosService,
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
