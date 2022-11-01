809c5b4cad60b3e24af16962fa34dcd3411a9ffe
Camila Zelenovsky <camila.zelenovsky@cat.com>
2022-11-01 15:48:09 -0300

    Serviço de dados

--- dashboard/src/app/dashboard/dados.service.ts
+++ dashboard/src/app/dashboard/dados.service.ts
@@ -1,9 +1,28 @@
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class DadosService {
 
+  readonly dados = [
+    ['Janeiro', 33],
+    ['Feveiro', 68],
+    ['Março', 49],
+    ['Abril', 15],
+    ['Maio', 80],
+    ['Junho', 27]
+  ];
+
   constructor() { }
+
+  //Retorna um observable contendo os dados a serem exibidos no gráfico com observable
+  obterDados(): Observable<any> {
+    return new Observable(observable => {
+      observable.next(this.dados);
+      observable.complete();
+    })
+  }
+
 }
