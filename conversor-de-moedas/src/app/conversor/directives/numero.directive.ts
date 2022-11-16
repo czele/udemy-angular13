643d2206032cc7889c7132ce771a32d1e4afb729
Camila Zelenovsky <camila.zelenovsky@cat.com>
2022-11-16 10:35:42 -0300

    Directives, precisa finalizar

--- conversor-de-moedas/src/app/conversor/directives/numero.directive.ts
+++ conversor-de-moedas/src/app/conversor/directives/numero.directive.ts
@@ -1,10 +1,32 @@
-import { Directive } from '@angular/core';
+import { Directive, HostListener } from '@angular/core';
+import { ControlValueAccessor, NG_VALUE_ACCESSOR } from '@angular/forms';
 
 @Directive({
-  selector: '[numero]'
+  selector: '[numero]',
+  providers: [{
+    provide: NG_VALUE_ACCESSOR,
+    useExisting: NumeroDirective,
+    multi: true
+  }]
 })
-export class NumeroDirective {
+export class NumeroDirective implements ControlValueAccessor {
 
   constructor() { }
+
+  //Implementa evento de keyup para o elemento da diretiva
+  @HostListener('keyup', ['$event'])
+  onKeyUp($event: any) {
+    let valor = $event.target.value;
+    let posDecimais = valor.indexOf('.');
+
+    valor = valor.replace(/[\D]/g, '');
+
+    if (posDecimais >0) {
+      valor = valor.substr(0, posDecimais) + '.' +
+        valor.substr(posDecimais);
+    }
+
+    $event.target.value = valor;
+  }
 
 }
